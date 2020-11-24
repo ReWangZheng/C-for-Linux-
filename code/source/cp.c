456aad149682eda110a195fff30a076e775d0ee8
Regan <29370330@qq.com>
2020-11-24 10:58:48 +0800

    本次提交编写了CP命令的简版

--- code/source/cp.c
+++ code/source/cp.c
@@ -7,15 +7,21 @@ void cp(char * src,char * des){
     int SRC_FD = open(src,O_RDONLY);
     if(SRC_FD==-1){
         perror("Can't open file!");
+        return;
+
     }
     
     int DES_FD = creat(des,0664);
     if(DES_FD==-1){
         perror("Can't open file!");
+        return;
     }
+    lseek(SRC_FD,30,SEEK_SET);
     char buf[1024];
     int read_size = 0;
     while((read_size=read(SRC_FD,buf,BUFSIZE))!=0){
         write(DES_FD,buf,read_size);
     }
+    close(SRC_FD);
+    close(DES_FD);
 }
--- code/source/main.c
+++ code/source/main.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 #include "cp.h"
 int main(int size,char* args[]){
-    who();
+    printf("%d\n",size);
+    if(size!=3){
+        printf("参数数量太少");
+        exit(1);
+    }
+    char * src = args[1];
+    char * des = args[2];
+    cp(src,des);
     return 0;
 }
