7c93653d61e9a9e1892a6caf423fd5d75c0beffa
Regan <29370330@qq.com>
2020-11-28 05:36:15 +0800

    暂时完成第七章的知识点

--- code/source/main.c
+++ code/source/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 #include "summary.h"
 int main(int size,char* args[]){
-    game_start();
+    newsigdemo();
 }
--- code/source/sigdemo.c
+++ code/source/sigdemo.c
@@ -12,5 +12,28 @@ void sigdemotest(){
         sleep(1);
         printf("hello\n");
     }
-    
+}
+
+void newhandle(int c){
+    printf("the interrupt has occur!");
+}
+
+void newsigdemo(){
+    struct sigaction nh;
+    char buf[10];
+    sigset_t blocked; //这是信号集合
+    nh.sa_flags = SA_RESETHAND|SA_RESTART;
+    nh.sa_handler=newhandle;
+    sigemptyset(&blocked);
+    sigaddset(&blocked,SIGQUIT);
+    nh.sa_mask = blocked;
+    if(sigaction(SIGINT,&nh,NULL)==-1){
+        perror("no sigaction!");
+    }else{
+        while (1)
+        {
+            fgets(buf,10,stdin);
+            printf("input:%s",buf);
+        }
+    }
 }
